49c3e756889677902b96dbcccbb0e10ace59ba02
Enock Nkumbuyedeni Nsanzimfura <nkumbuyenock@gmail.com>
2024-06-10 23:02:27 +0200

    Update mockData.ts

--- src/util/mockData.ts
+++ src/util/mockData.ts
@@ -35,28 +35,6 @@ export const user: User = {
                     "date": "2024-05-04",
                     "description": "To complete Flutter navigation app",
                     "isCompleted": false,
-                    "subTasks": [
-                        {
-                            "id": "1",
-                            "isCompleted": false,
-                            description: "How to use graphql in flutter app"
-                        },
-                        {
-                            "id": "2",
-                            "isCompleted": false,
-                            description: "How to use graphql in flutter app"
-                        },
-                        {
-                            "id": "3",
-                            "isCompleted": false,
-                            description: "How to use graphql in flutter app"
-                        },
-                        {
-                            "id": "4",
-                            "isCompleted": false,
-                            description: "How to use graphql in flutter app"
-                        },
-                    ]
                 },
             ]
         },
